372f87c5021fe5e161aba88a891008191dabe878
colint1499 <114102584+colint1499@users.noreply.github.com>
2022-12-14 15:19:53 -0700

    Create problem2.ino

---
+++ problem2.ino
@@ -0,0 +1,18 @@
+/*
+
+*/
+
+void setup() {
+    pinMode(greenpin,OUTPUT);
+    pinMode(redpin,OUTPUT);
+    pinMode(bluepin,OUTPUT);
+    
+    analogWrite(bluepin, 135);
+    analogWrite(redpin, 255);
+    analogWrite(greenpin, 0);
+    
+}
+
+void loop() {
+    
+}
